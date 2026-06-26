85dfe379b580c07361461ebda835d449f2e519bc
matahari999 <163366505+matahari999@users.noreply.github.com>
2026-06-26 14:43:00 +0900

    fix: add missing showcase module

---
+++ src/lib/showcase.ts
@@ -0,0 +1,33 @@
+export interface ShowcaseCouple {
+  id: string
+  groom: string
+  bride: string
+  date: string
+  dateDisplay: string
+  template: string
+  photoIdx: number
+  subdomain: string
+}
+
+export const showcaseCouples: ShowcaseCouple[] = [
+  { id: 'nolan-jenny', groom: 'Xuân Nam (Nolan)', bride: 'Kim Thơ (Jenny)', date: '08/08/2026', dateDisplay: '08/08/2026', template: 'classic-red', photoIdx: 1, subdomain: 'nolan-jenny' },
+  { id: 'hoang-duong-giang', groom: 'Hoàng Dương', bride: 'Trần Giang', date: '13/06/2026', dateDisplay: '13/06/2026', template: 'minimal-beige', photoIdx: 2, subdomain: 'hoangduong-trangiang' },
+  { id: 'van-tai-truc-linh', groom: 'Văn Tài', bride: 'Trúc Linh', date: '26/07/2026', dateDisplay: '26/07/2026', template: 'romantic-pink', photoIdx: 3, subdomain: 'vantai-truclinh' },
+  { id: 'phuc-khai-kha-tu', groom: 'Phúc Khải', bride: 'Khả Tú', date: '20/06/2026', dateDisplay: '20/06/2026', template: 'gold-elegant', photoIdx: 4, subdomain: 'friends-lovers-married' },
+  { id: 'nhat-truong-thu-ai', groom: 'Nhật Trường', bride: 'Thu Ái', date: '23/07/2026', dateDisplay: '23/07/2026', template: 'sage-green', photoIdx: 5, subdomain: 'nhattruong-thuai' },
+  { id: 'xuan-kien-vu-duyen', groom: 'Xuân Kiên', bride: 'Vũ Duyên', date: '20/06/2026', dateDisplay: '20/06/2026', template: 'vintage-rose', photoIdx: 6, subdomain: 'xuankien-vuduyen' },
+  { id: 'ngoc-ly', groom: 'Minh Ngọc', bride: 'Cẩm Ly', date: '30/05/2026', dateDisplay: '30/05/2026', template: 'french-vintage', photoIdx: 7, subdomain: 'ngocly' },
+  { id: 'hong-minh-nguyen-linh', groom: 'Hồng Minh', bride: 'Nguyễn Linh', date: '06/06/2026', dateDisplay: '06/06/2026', template: 'ao-dai', photoIdx: 8, subdomain: 'hongminh-linh' },
+  { id: 'hoang-muong', groom: 'Văn Hoàng', bride: 'Phượng Muông', date: '27/05/2026', dateDisplay: '27/05/2026', template: 'rustic-green', photoIdx: 9, subdomain: 'hoangmuong' },
+  { id: 'dat-trang', groom: 'Đạt', bride: 'Trang', date: '27/05/2026', dateDisplay: '27/05/2026', template: 'minimal-black', photoIdx: 10, subdomain: 'thiep-cuoi-dat-trang' },
+  { id: 'hong-phuc-gia-tue', groom: 'Hồng Phúc', bride: 'Gia Tuệ', date: '22/05/2026', dateDisplay: '22/05/2026', template: 'rose-gold', photoIdx: 11, subdomain: 'phuc-tue' },
+  { id: 'viet-anh-hai-yen', groom: 'Việt Anh', bride: 'Hải Yến', date: '10/05/2026', dateDisplay: '10/05/2026', template: 'dong-son', photoIdx: 12, subdomain: 'haiyen-vietanh' },
+  { id: 'duy-linh', groom: 'Bùi Duy', bride: 'Diệu Linh', date: '25/05/2026', dateDisplay: '25/05/2026', template: 'watercolor', photoIdx: 13, subdomain: 'duylinh' },
+  { id: 'thanh-linh', groom: 'Đức Thành', bride: 'Thùy Linh', date: '10/05/2026', dateDisplay: '10/05/2026', template: 'peony-garden', photoIdx: 14, subdomain: 'linhthanh' },
+  { id: 'tuan-anh', groom: 'Hải Tuấn', bride: 'Kim Anh', date: '30/04/2026', dateDisplay: '30/04/2026', template: 'black-gold', photoIdx: 15, subdomain: 'kimanh-haituan' },
+  { id: 'do-thanh', groom: 'Phạm Đô', bride: 'Giang Thanh', date: '10/05/2026', dateDisplay: '10/05/2026', template: 'hoa-sen', photoIdx: 16, subdomain: 'phamdo-thanh' },
+  { id: 'bach-my', groom: 'Văn Bách', bride: 'Trà My', date: '19/04/2026', dateDisplay: '19/04/2026', template: 'earthy-beige', photoIdx: 17, subdomain: 'vanbach-tramy' },
+  { id: 'minh-linh', groom: 'Công Minh', bride: 'Thùy Linh', date: '03/05/2026', dateDisplay: '03/05/2026', template: 'olive-garden', photoIdx: 18, subdomain: 'congminh-linh' },
+  { id: 'vu-diem-linh', groom: 'Nguyễn Vũ', bride: 'Diễm Linh', date: '06/06/2026', dateDisplay: '06/06/2026', template: 'classic-red', photoIdx: 19, subdomain: 'vu-linh' },
+  { id: 'huyen-tien', groom: 'Bá Huyên', bride: 'Tiên', date: '26/04/2026', dateDisplay: '26/04/2026', template: 'truc-dong', photoIdx: 20, subdomain: 'huyen-tien' },
+]
